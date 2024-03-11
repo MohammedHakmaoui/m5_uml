6e650439f7d31a261007d7228ed8ed0269dde915
MohammedHakmaoui <145457006+MohammedHakmaoui@users.noreply.github.com>
2024-03-11 09:35:52 +0100

    Update uml a python.py

--- uml a python.py
+++ uml a python.py
@@ -1,17 +1,17 @@
-class Client:
+class Client: 
     def __init__(self, nom: str, adreca: str, telefon: str, email: str):
         # Inicialitzem els atributs de la classe Client
         self.nom = nom  # Nom del client
         self.adreca = adreca  # Adreça del client
-        self.telefon = telefon  # Número de telèfon del client
-        self.email = email  # Correu electrònic del client
+        self.telefon = telefon  # Número de telefon del client
+        self.email = email  # Correu electronic del client
 
 class UsuariWeb:
     def __init__(self, login: str, password: str, estat: str):
         # Inicialitzem els atributs de la classe UsuariWeb
         self.login = login  # Nom d'usuari per iniciar sessió
         self.password = password  # Contrasenya per iniciar sessió
-        self.estat = estat  # Estat de l'usuari (per exemple: actiu, inactiu)
+        self.estat = estat  # Estat de l'usuari (actiu, inactiu...)
 
 class CarroCompra:
     def __init__(self, client: Client, factura: 'Factura'):
@@ -34,14 +34,14 @@ class Factura:
         self.adreca_facturacio = adreca_facturacio  # Adreça de facturació
         self.data_emissio = data_emissio  # Data d'emissió de la factura
         self.data_tancament = data_tancament  # Data de tancament de la factura
-        self.pagament = pagament  # Mètode de pagament de la factura
+        self.pagament = pagament  # Metode de pagament de la factura
 
 class LiniaComanda:
     def __init__(self, producte: Producte, quantitat: int, preu: float):
         # Inicialitzem els atributs de la classe LiniaComanda
         self.producte = producte  # Producte associat a la línia de comanda
         self.quantitat = quantitat  # Quantitat del producte
-        self.preu = preu  # Preu unitari del producte
+        self.preu = preu  # Preu per producte
 
 class OrdreEnviament:
     def __init__(self, codi_identificatiu: str, data_comanda: str, data_enviament: str, destinacio: str, import_: float, estat: str):
